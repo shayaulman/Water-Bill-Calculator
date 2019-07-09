b4fab5d8f50c3494eba6509d09ee77b6dfb5969d
Shaye Ulman <shayeulman@gmail.com>
2019-07-09 14:48:35 +0300

    Some serious cleanups

--- src/app.js
+++ src/app.js
@@ -4,81 +4,69 @@ function Calculation () {
         return type === 'num' ? +e.value : e.value;
     });    
    
-    this.info = () => {
+    this.info = () => { 
         [
-            this.names,
-            this.amountOfPeople,
-            this.tariffs,
-            this.billUsage,
-            this.meters,
+            names,
+            amountOfPeople,
+            tariffs,
+            billUsage,
+            meters,
         ] = [
             ['.family-names', 'str'],
             ['.amount-of-people', 'num'],
             ['.prices', 'num'],
             ['.usage-amount', 'num'],
             ['.meters input', 'num'],
-    
+        
         ].map((c) => this.selectClasses(c[0],`${ c[1] === 'num' ? 'num':'str'}`));
-
-        this.usage = this.meters.map((a,i,t) => i%2 !==0 ? a-(t[i-1]): null).filter(n => n !== null);
-        this.usage.unshift(this.billUsage.reduce((a,v) => a+v, 0) - this.usage.reduce((a,v) => a+v, 0));
-
+        
+        usage = meters.map((a,i,t) => i%2 !==0 ? a-(t[i-1]): null).filter(n => n !== null);
+        // calculate usage of  main consumer & add to array
+        usage.unshift(billUsage.reduce((a,v) => a+v, 0) - usage.reduce((a,v) => a+v, 0));  
+        
         return ({
-            names: this.names,
-            amountOfPeople: this.amountOfPeople,
-            tariffs: this.tariffs,
-            billUsage: this.billUsage,
-            usage: this.usage,
-            meters: this.meters
+            names,
+            amountOfPeople,
+            tariffs,
+            billUsage,
+            usage,
+            meters
         });
     }
-
-
+    
     this.result = () => {
-        
         const allowedAmountForPerson = this.info().billUsage[0] / this.info().amountOfPeople.reduce((a,v) => a+v, 0);
         const allowedForEachTenant = this.info().amountOfPeople.map(t => t * allowedAmountForPerson);
         
 
         const tariff1ToPay = allowedForEachTenant.map(a => a * this.info().tariffs[0]);
-        const tariff2ToPay = this.usage.map((p, i) => (p - allowedForEachTenant[i]) * this.tariffs[1])
+        const tariff2ToPay = this.info().usage.map((p, i) => (p - allowedForEachTenant[i]) * this.info().tariffs[1])
         const totalToPay = tariff1ToPay.map((t, i) => t+ tariff2ToPay[i]);
-        console.log(tariff2ToPay)
-        console.log(this.usage)
-        return (
-            {
-                tariff1ToPay,
-                tariff2ToPay,
-                totalToPay,
-                allowedForEachTenant
-            }
-        )
-        
-        // allowedForEachTenant.map(a => a * this.info().tariffs[1]); 
-        console.log('allowedForEachTenant:' ,allowedForEachTenant)
-        console.log('tariff1ToPay:', tariff1ToPay)
-        console.log('tariff2ToPay', tariff2ToPay)
 
+        return ({
+            allowedForEachTenant,
+            tariff1ToPay,
+            tariff2ToPay,
+            totalToPay
+        })
     }
 }
 
 
-function testa() {
-    const test = new Calculation();
-    console.log(test.info())
-    console.log(test.result())
+function calculate() {
+    const calculation = new Calculation();
     let table = document.querySelector('table');
     html = '';
-    test.names.forEach((n, i) => html += `
+    calculation.info().names.forEach((n, i) => html += `
         <tr>
             <td rowspan="2">${n}</td>
             <td>תעריף 1</td>
-            <td>${test.result().allowedForEachTenant[i].toFixed(2)} מ"ק</td>
-            <td rowspan="2">${test.result().totalToPay[i].toFixed(2)} ‎₪</td>
+            <td>${calculation.result().allowedForEachTenant[i].toFixed(2)} מ"ק</td>
+            <td rowspan="2">${calculation.result().totalToPay[i].toFixed(2)} ‎₪</td>
         </tr>
         <tr>
             <td>תעריף 2</td>
-            <td>${(test.usage[i] - test.result().allowedForEachTenant[i]).toFixed(2)} מ"ק</td> 
+            <td>${(calculation.info().usage[i] - calculation.result().allowedForEachTenant[i]).toFixed(2)} מ"ק</td> 
         </tr>`
     );
 
